b5a4f3337e2816f04a00209b5ca347d18c8c7bc7
andersondelfino <andersonaugusto808@gmail.com>
2024-05-22 16:30:12 -0300

    adicionar operaçoes

---
+++ Tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+   public static void main(String[] args) {
+ 
+    int a , b;
+    a = 5;
+    b = 6;
+
+    boolean resultado = a == b ? true : false;
+
+    System.out.println(resultado);
+   } 
+}
