ab9091ecdac2667977373fb3221aa2396c9d6194
andersondelfino <andersonaugusto808@gmail.com>
2024-05-22 14:00:19 -0300

    criando o projeto para exemplo de tipos variaveis

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
-public class App {
+public class TiposVariaveis {
+   
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*representação de Strings */
+        String meuNome = "Anderson";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        /*A variavel vai ser aletrada normalmente */
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI =3.14;
+        /*O valor de PI nao vai ser aletrado, pois a variavel foi declarada com "final" */
+        
+    
     }
 }
