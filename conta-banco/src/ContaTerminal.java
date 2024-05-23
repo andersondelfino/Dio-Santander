327483268cfac74d77e5d6a46cce4a7524d5fb4c
andersondelfino <andersonaugusto808@gmail.com>
2024-05-23 14:39:29 -0300

    primeiro projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe Scanner 
+        
+        //Exibir as mensagens para o nosso usuario 
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+        double saldo = 15;
+        double valorSolicidado = 22;
+
+        
+
+        if ( saldo > valorSolicidado){
+            saldo = saldo - valorSolicidado;
+        } else{
+            System.out.println("Saldo insuficiente");
+        }
+        System.out.println(saldo);
     }
 }
