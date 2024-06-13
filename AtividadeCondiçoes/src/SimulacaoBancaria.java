8ba579f6bd60ddbba67f28f01cfcaf30d04d4527
andersondelfino <andersonaugusto808@gmail.com>
2024-06-13 19:32:58 -0300

    Aividade controle de fluxo desafios

---
+++ AtividadeCondiçoes/src/SimulacaoBancaria.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:
+                    double valorSaque = scanner.nextDouble();
+                    if (saldo >= valorSaque) {
+                        saldo -= valorSaque;
+                        System.out.println("Saldo atual: " + saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
+
