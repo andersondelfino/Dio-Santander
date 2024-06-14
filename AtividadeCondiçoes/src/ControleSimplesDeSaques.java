0ad7cd8cec1d24883ebb2906f4b383453b45a09d
andersondelfino <andersonaugusto808@gmail.com>
2024-06-14 11:37:21 -0300

    atualização

--- AtividadeCondiçoes/src/ControleSimplesDeSaques.java
+++ AtividadeCondiçoes/src/ControleSimplesDeSaques.java
@@ -5,25 +5,24 @@ public class ControleSimplesDeSaques {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o limite diário de saque:");
         double limiteDiario = scanner.nextDouble();
         
-        // Loop for para iterar sobre os saques
-        for (int i = 1;; i++) {
-
+        // Loop para iterar sobre os saques
+        while (true) {
+            System.out.println("Digite o valor do saque (0 para encerrar):");
             double valorSaque = scanner.nextDouble();
 
             if (valorSaque == 0) {
-                System.out.println("Transacoes encerradas.");
+                System.out.println("Transações encerradas.");
                 break; // Encerra o loop
             } else if (valorSaque > limiteDiario) {
-                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                System.out.println("Limite diário de saque atingido. Transações encerradas.");
                 break; // Encerra o loop
             } else {
                 if (valorSaque < limiteDiario) {
                     limiteDiario -= valorSaque;
                     System.out.println("Saque realizado. Limite restante: " + limiteDiario);
-                    System.out.println("Transacoes encerradas.");
-                    break;
                 } 
             }
         }
